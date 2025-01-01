30810236e6ab64452156b2510dcc3f532ace7b07
sangeet2021 <stiletdurden252@gmail.com>
2025-01-02 01:16:26 +0545

    build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/E-comDep/', // Add this line with your repository name
+  build: {
+    outDir: 'build', // Set the output directory to 'build'
+  },
+});
